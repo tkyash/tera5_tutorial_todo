fc00951860099b26b60467c796c81557791dccad
honzan <honzan0821@gmail.com>
2015-10-13 22:48:35 +0900

    Revert "同じファイルの箇所違いのコミットテスト"
    This reverts commit 748d91ab579943ea2c60cccd2af92682c0e8efc7.

--- src/main/java/todo/app/welcome/HomeController.java
+++ src/main/java/todo/app/welcome/HomeController.java
@@ -27,8 +27,6 @@ public class HomeController {
     public String home(Locale locale, Model model) {
         logger.info("Welcome home! The client locale is {}.", locale);
 
-        //足しました
-
         Date date = new Date();
         DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
                 DateFormat.LONG, locale);
